aaa2bc2e924a4f465c73a407f1dda126f8b69b52
Stefan Mixdorf <stefan.mixdorf@netgo.de>
2024-07-05 10:43:24 +0200

    Projekt aufgesetzt

---
+++ backend/src/main/java/de/netgo/wochenplan/WochenplanApplication.java
@@ -0,0 +1,13 @@
+package de.netgo.wochenplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WochenplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WochenplanApplication.class, args);
+	}
+
+}
